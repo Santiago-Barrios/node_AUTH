12e0f48dfa457764616932d49903e939305d74a1
Santiago Barrios <santiagobarriosgrijalba.08@gmail.com>
2022-02-28 08:35:34 -0500

    make it better

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express =  require('express');
 const cors = require('cors');
+const path = require('path');
 const { dbConnection } = require('./db/config');
 require('dotenv').config();
 
@@ -21,6 +22,11 @@ app.use( express.json() );
 
 // Rutes
 app.use( '/api/auth', require('./routes/auth') );
+
+// manage all the rutes
+app.get('*', (req, res) => {
+    res.sendFile( path.resolve( __dirname, 'public/index.html' ) );
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`server is runnig in port ${ process.env.PORT }`);
